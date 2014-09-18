1f3031899fc44ec6a0bff92b7e3a684814c29fc9
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-09-18 20:30:33 +0200

    Addeed tag to clan

--- test_infected_no_compo.sql
+++ test_infected_no_compo.sql
@@ -40,6 +40,7 @@ CREATE TABLE IF NOT EXISTS `clans` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `chief` int(11) NOT NULL,
   `name` varchar(32) NOT NULL,
+  `tag` varchar(32) NOT NULL,
   `event` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
