4618cb13f6f2faf7cc139901da9bf35d9bfb7be2
Liam Svanåsbakken Crouch <petterroea@skymiastudios.com>
2015-02-15 23:23:26 +0100

    Forgot to commit these changes

--- test_infected_no_compo.sql
+++ test_infected_no_compo.sql
@@ -117,6 +117,7 @@ CREATE TABLE IF NOT EXISTS `matches` (
   `winner` int(11) NOT NULL,
   `compoId` int(11) NOT NULL,
   `bracketOffset` int(8) NOT NULL,
+  `chat` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
